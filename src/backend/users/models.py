371d3388f30a25ac01a4d99a7de3b6c2cc8bdc64
Benoît Durand <benoit.durand.mail@gmail.com>
2022-06-08 23:03:16 +0200

    Update commit

--- src/backend/users/models.py
+++ src/backend/users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 from django.contrib.auth.base_user import BaseUserManager
-from django.utils.translation import ugettext_lazy as _
 
 
 class CustomUserManager(BaseUserManager):
@@ -13,7 +12,7 @@ class CustomUserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
 
         if not email:
-            raise ValueError(_("The Email must be set"))
+            raise ValueError("The Email must be set")
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -27,15 +26,15 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault("is_active", True)
 
         if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff=True."))
+            raise ValueError("Superuser must have is_staff=True.")
         if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
+            raise ValueError("Superuser must have is_superuser=True.")
         return self.create_user(email, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
     username = None
-    email = models.EmailField(_("email address"), unique=True)
+    email = models.EmailField("email address", unique=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
