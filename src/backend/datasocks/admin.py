aa8e988bfd914e3b933c521dd60a917d48751623
Benoît Durand <benoit.durand.mail@gmail.com>
2022-06-12 22:42:08 +0200

    Fixtures and admin view

--- src/backend/datasocks/admin.py
+++ src/backend/datasocks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Dashboard,Button,Card,DataRecord
 # Register your models here.
+admin.site.register(Dashboard)
+admin.site.register(Button)
+admin.site.register(Card)
+admin.site.register(DataRecord)
