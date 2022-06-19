1c65c6a71737b58f6e9cd4ccd440c9699a3e3e24
Benoît Durand <benoit.durand.mail@gmail.com>
2022-06-19 15:11:03 +0200

    Add tests

--- src/backend/datasocks/tests.py
+++ src/backend/datasocks/tests.py
@@ -1,11 +1,15 @@
 from django.test import Client
 from django.test import TestCase
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
 
 HTTP_STATUS_CODE_FORBIDDEN = 403
 HTTP_STATUS_CODE_UNAUTHORIZED = 401
 
+HTTPS_STATUS_CODE_SUCCESS = 200
+
 # Create your tests here.
-class DashboardTest(TestCase):
+class NoAuthTest(TestCase):
     fixtures = ["fixture.yaml"]
 
     def test_get_dashboard_api(self):
@@ -28,4 +32,20 @@ class DashboardTest(TestCase):
         response = client.get('/api/1/graphs/1/')
         self.assertEqual(response.status_code,HTTP_STATUS_CODE_UNAUTHORIZED)
 
-    
+    def test_get_data_api(self):
+        client = Client()
+        response = client.get('/api/1/data/')
+        self.assertEqual(response.status_code,HTTP_STATUS_CODE_UNAUTHORIZED)
+
+class User1Test(TestCase):
+
+    fixtures = ["fixture.yaml"]
+
+    def setUp(self):
+        token = Token.objects.get(user__email='user1@test.com')
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+    def test_get_dashboard_1(self):
+        response = self.client.get('/api/1/dashboards/1/')
+        self.assertEqual(response.status_code,HTTPS_STATUS_CODE_SUCCESS)
