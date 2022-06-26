aa11a29ff54e36af709c8af94ed7d2de9f76061b
duraben <benoit.durand.mail@gmail.com>
2022-06-26 16:00:03 +0000

    Deploy fix

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-DEBUG = True
+DEBUG = os.environ["DEBUG"]
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production
@@ -72,7 +72,10 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+url = "http://" + os.environ['HOST']
+CORS_ALLOWED_ORIGINS = [
+    url
+]
 
 ROOT_URLCONF = "backend.urls"
 
